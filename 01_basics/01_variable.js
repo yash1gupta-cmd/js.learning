a455da64bfb8bc70c5e80c680048d3040ea62e37
yash1gupta-cmd <yashg17572@gmail.com>
2026-07-25 12:31:06 +0000

    change done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const userID = 12345
+let userName = "yash"
+var userpassword = "11111"
+usercity = "jaipur"
+let userstate;
+
+// userID = 20030// this id const key not changeable
+userName = "hello"
+userpassword ="22222"
+usercity ="UP"
+
+console.log( userID);
+
+console.table([userID , userName , userpassword ,usercity , userstate])
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,13 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(2+2) // we are working on nodejs not browser
+
+console.log(3+3); //code readability should be high
+
+console.log("yash");
+
+console.log(typeof undefined); //undefined
+
+console.log(typeof null); //object
+
+

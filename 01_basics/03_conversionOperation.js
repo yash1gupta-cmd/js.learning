15ee4872439c51c9b8dfe69e0675a70748228601
yash1gupta-cmd <yashg17572@gmail.com>
2026-07-26 16:59:33 +0000

    part1 coversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,40 @@
+// let score = "abc"
+// let score =null
+// let score =undefined
+// let score =true
+
+
+
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
+
+
+// notes--
+// "33" => 33
+// "33abc" => NaN
+// true =>1; false =>0
+
+// let isLoggedIn = "yash"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+//  console.log(booleanIsLoggedIn);
+
+
+ // 1=> true; 0=> false
+ //"" => false
+ //"yash" => true
+
+ let somenumber = 19
+
+ let Stringnumber = String(somenumber);
+ console.log(Stringnumber);
+ console.log(typeof Stringnumber);
+
+
+
+ 
+
+ 

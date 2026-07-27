f175e2325fdc38d14eab67a46282fe793b137f79
yash1gupta-cmd <yashg17572@gmail.com>
2026-07-27 07:53:07 +0000

    done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,51 @@
+// console.log(2 > 1);
+// console.log( 2 >= 1);
+// console.log( 2 < 1);
+// console.log( 2 == 1);
+// console.log( 2 != 1);
+
+
+// console.log("2" > 1 );
+// console.log("02" > 1 );
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// comparision like this is not very good to use
+
+//=== is the strict check thats even check the data type like its string is or not
+
+// console.log( "2" === 2);
+
+
+// let name = "mynameisyash"
+
+// let idname = name
+
+// idname = "eyepain"
+
+// console.log(idname);
+// console.log(idname);
+
+
+let userone = {
+    efmail: "yash"
+     upi: "2213"
+
+}
+let usertwo = userone
+usertwo.email= "sdasd"
+console.log(userone.email);
+console.log(usertwo.email);
+
+
+
+
+
+
+
+
+
+
+

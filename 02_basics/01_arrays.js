53e6755afabf4c18b059ead966fce56c14ede351
yash1gupta-cmd <yashg17572@gmail.com>
2026-08-01 08:22:09 +0000

    done

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,32 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeros =["spiderman","deadpool" ]
+
+const myArr1 = new Array(1,2,3,4)
+
+console.log(myArr[1]);
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+// .pop remove the last array
+
+myArr.unshift(9)
+
+// .upshift add array in start
+
+myArr.shift()
+
+// thats remove array from start
+
+
+// console.log(myArr.includes(9));
+
+console.log(myArr.indexOf(3));
+
+
+console.log(myArr);
+

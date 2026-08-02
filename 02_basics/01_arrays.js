461542d83af9878952e56d73d088cf8a751be1d2
yash1gupta-cmd <yashg17572@gmail.com>
2026-08-02 12:56:34 +0000

    change done

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -5,13 +5,16 @@ const myHeros =["spiderman","deadpool" ]
 
 const myArr1 = new Array(1,2,3,4)
 
-console.log(myArr[1]);
+// console.log(myArr[1]);
 
 // Array methods
 
 // myArr.push(6)
 // myArr.push(7)
+// add Element in the last
 // myArr.pop()
+// remove Element in the last 
+
 // .pop remove the last array
 
 myArr.unshift(9)
@@ -25,8 +28,40 @@ myArr.shift()
 
 // console.log(myArr.includes(9));
 
-console.log(myArr.indexOf(3));
+// console.log(myArr.indexOf(3));
+
+const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log( typeof newArr);
 
 
-console.log(myArr);
+//slice , splice
+
+console.log("A" , myArr);
+
+
+const myn1 = myArr.slice(1, 3)
+console.log(myn1);
+console.log("B ", myArr);
+// slice don't manupulate the orginal array
+
+
+const myn2 = myArr.splice(1,3)
+console.log("C ", myArr);
+console.log(myn2);
+// splice manupulate the orginal array
+
+
+
+
+
+
+
+
+
+
+
+
+
 

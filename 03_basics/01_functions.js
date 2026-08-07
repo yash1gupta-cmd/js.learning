42ab15195e581c8cd4c7e0855b8efe91fff95be6
yash1gupta-cmd <yashg17572@gmail.com>
2026-08-07 13:58:43 +0000

    i try tommorow

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,20 @@
+function sayMyName() {
+    console.log("y");
+    console.log("a");
+    console.log("s");
+    console.log("h");
+}
+
+// sayMyName this is only Reference
+// sayMyName() this  execute
+
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2);
+    
+// }
+function addTwoNumbers(number1, number2){
+    let res
+    
+}
+
+addTwoNumbers(3,4)

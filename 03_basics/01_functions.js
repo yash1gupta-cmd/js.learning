8fcffc7fa3bd01a508f0b9b1526dfaa76fc8dd7c
yash1gupta-cmd <yashg17572@gmail.com>
2026-08-08 17:09:04 +0000

    today done

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -10,11 +10,32 @@ function sayMyName() {
 
 // function addTwoNumbers(number1, number2){
 //     console.log(number1 + number2);
-    
+
 // }
-function addTwoNumbers(number1, number2){
-    let res
-    
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2);
+// }
+function addTwoNumbers(number1, number2) {
+    let result = number1 + number2
+    return result
+    //  console.log("yash");
+    //  after return nothing got print 
+
+
 }
 
-addTwoNumbers(3,4)
+const result = addTwoNumbers(3, 4)
+//  console.log("Result: ", result);
+
+
+function loginUserMessage(username) {
+    if (username === undefined) {
+        console.log("enter your username");
+        return
+        
+    }
+    return `${username} just logged in `
+}
+// console.log(loginUserMessage("username"))
+console.log(loginUserMessage())
+

b7de32d7f5a78ee053532ee9fbb930840c059029
yash1gupta-cmd <yashg17572@gmail.com>
2026-08-29 08:02:21 +0000

    done

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,10 @@
+const user = {
+     username: "hitesh",
+     price: 999,
+
+     welcomeMessage: function(){
+        console.log(`${this.username} , Welcome to website`);
+        
+     }
+}
+user.welcomeMessage()

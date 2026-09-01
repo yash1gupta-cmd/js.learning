baf0b4df8d8edfe0193a579411a45913d327d113
yash1gupta-cmd <yashg17572@gmail.com>
2026-09-01 08:17:20 +0000

    arrow function done

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -4,7 +4,50 @@ const user = {
 
      welcomeMessage: function(){
         console.log(`${this.username} , Welcome to website`);
+      //   console.log(this);
+        
         
      }
 }
-user.welcomeMessage()
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+// console.log(this);
+
+// function chai() {
+//    let  username = "yash"
+//    console.log(this.username);
+// }
+// chai()
+
+// const chai = function () {
+//    let username = "yash"
+//    console.log(this.username);
+// }
+
+
+const chai =  () => {
+   let username = "yash"
+   console.log(this);
+}
+
+// chai()
+
+// const addnum = (num1, num2) =>{
+//    return num1 +num2 
+// }
+// using return called explicit return and not using called implicit return 
+
+// if you use cruly braces than you have to use return 
+
+// const addnum = (num1, num2) => num1 +num2 
+
+// const addnum = (num1, num2) => (num1 +num2 )
+
+// for objects where you need to add curly barces 
+const addnum = (num1, num2) => ({username: "yash"})
+
+// console.log(addnum(3,4));
+console.log(addnum());
+
+

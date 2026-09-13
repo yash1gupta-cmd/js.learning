22660fe5aeee091e2fe90f0ba1f140de10c8c643
yash1gupta-cmd <yashg17572@gmail.com>
2026-09-13 12:31:21 +0000

    switch done

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,51 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3
+// switch (month) {
+//     case 1:
+//         console.log("january");
+//         break;
+//     case 2:
+//         console.log("feb");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;
+//     case 4:
+//         console.log("april");
+//         break;
+
+//     default :
+//         console.log("default case match");
+        
+//         break;
+// }
+
+
+const month = "jan"
+switch (month) {
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default :
+        console.log("default case match");
+        
+        break;
+}

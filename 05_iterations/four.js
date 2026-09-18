31f9c5acbf3764f17b2d0b1e3aae4d335901d5d6
yash1gupta-cmd <yashg17572@gmail.com>
2026-09-18 04:43:54 +0000

    done

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -5,6 +5,12 @@ const myobject = {
      swift: "swift for apple",
 }
 for (const key in myobject) {
-    console.log(`${key} shortcut is for ${myobject[key]}`);
+    // console.log(`${key} shortcut is for ${myobject[key]}`);
     
 }
+
+ const programming = ["js","rb","py","java","cpp" ]
+ for (const key in programming) {
+    console.log(programming[key]);
+    
+ }
